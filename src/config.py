fdc7b1ab553675337fbb0bc628208456a5e92695
VkTheEncoder <158612790+VkTheEncoder@users.noreply.github.com>
2025-06-30 10:21:06 +0530

    Update config.py

--- src/config.py
+++ src/config.py
@@ -5,4 +5,5 @@ load_dotenv()
 
 API_ID   = int(os.getenv('API_ID', 0))
 API_HASH = os.getenv('API_HASH', '')
+BOT_TOKEN = os.getenv('BOT_TOKEN', None)   
 LOG_LEVEL = os.getenv('LOG_LEVEL', 'INFO').upper()
