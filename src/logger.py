63d81e28da0f25ca186fba3aad90454669fc1da6
VkTheEncoder <158612790+VkTheEncoder@users.noreply.github.com>
2025-06-30 10:52:31 +0530

    Update logger.py

--- src/logger.py
+++ src/logger.py
@@ -1,8 +1,10 @@
+# src/logger.py
+
 import logging
 from config import LOG_LEVEL
 
 logging.basicConfig(
-    format='%(asctime)s [%(levelname)s] %(message)s',
+    format="%(asctime)s [%(levelname)s] %(message)s",
     level=LOG_LEVEL
 )
 logger = logging.getLogger(__name__)
